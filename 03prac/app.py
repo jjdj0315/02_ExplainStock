13d0232a15adf90123fcb333915a48e930d4dc25
DAEJIN JEONG <jjdj0315@gmail.com>
2025-05-18 19:51:44 +0900

    Study : class searchResult

--- 03prac/app.py
+++ 03prac/app.py
@@ -1,7 +1,11 @@
 import streamlit as st
-
+from utils.search import stock_search
+from utils.SearchResult import SearchResult
 
 st.title("주식 정보 분석 대시보드")
 
+
+
 search_query = st.text_input("검색창")
 hits =stock_search(search_query)["hits"]
+search_results = [SearchResult(hit) for hit in hits]
---
+++ 03prac/utils/SearchResult.py
@@ -0,0 +1,14 @@
+class SearchResult:
+    def __init__(self, item):
+        self.item = item
+
+    @property
+    def symbol(self):
+        return self.item["Symbol"]
+
+    @property
+    def name(self):
+        return self.item["Name"]
+
+    def __str__(self):
+        return f"{self.symbol} : {self.name}"
